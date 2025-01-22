290d8b2f6a604bce7d597aa277551ec1da231855
James Tauber <jtauber@jtauber.com>
2025-01-22 14:09:15 -0500

    test suite updated to 0.4 spec

--- validator.py
+++ validator.py
@@ -27,40 +27,43 @@ def validate(filename):
 
     if data.get("format") != "alignment":
         return (1, 'Must have "format": "alignment"')
-    if data.get("version") not in ["0.3", "0.3.1"]:
-        return (2, 'Must have "version" that is in ["0.3", "0.3.1"]')
-    if ("groups" in data and "records" in data) or not ("groups" in data or "records" in data):
-        return (3, 'Must have either "groups" or "records" but not both')
+    if data.get("version") not in ["0.4"]:
+        return (2, 'Must have "version" that is in ["0.4"]')
+    if "groups" not in data:
+        return (3, 'Must have "groups"')
     if "groups" in data and not isinstance(data["groups"], list):
-        return (4, 'If "groups" is present, it must be a list')
-    if "records" in data and not isinstance(data["records"], list):
-        return (5, 'If "records" is present, it must be a list')
+        return (4, '"groups" must be a list')
+    if "records" in data:
+        return (5, '"records" is not allowed at the top level in this version of the format')
 
-    for record in data.get("records", []):
-        if not isinstance(record, dict):
-            return (6, 'Each record must be a dictionary')
-        if "type" not in record:
-            return (7, 'Each record must have a "type"')
-        if not isinstance(record.get("meta", {}), dict):
-            return (8, 'If "meta" is present on a record, it must be a dictionary')
-        if not isinstance(record.get("references", []), list):
-            return (9, 'If "references" is present on a record, it must be a list')
-        if "references" not in record and not [key for key in record.keys() if key not in ["type", "meta"]]:
-            return (10, 'If "references" is not present on a record, it must have other keys besides "type" and "meta"')
-        if "references" in record and [key for key in record.keys() if key not in ["type", "meta", "references"]]:
-            return (11, 'If "references" is present on a record, it must not have other keys besides "type", "meta", and "references"')
-        for key, value in record.items():
-            if key not in ["type", "meta", "references"]:
-                # unit role
-                result = validate_reference_unit(value)
-                if result is not None:
-                    return result
-        if "references" in record:
-            if record["references"] == []:
-                return (14, 'A references list cannot be empty')
-            for reference_unit in record["references"]:
-                result = validate_reference_unit(reference_unit)
-                if result is not None:
-                    return result
+    for group in data["groups"]:
+        if not isinstance(group, dict):
+            return (19, 'Each group must be a dictionary')
+        for record in group.get("records", []):
+            if not isinstance(record, dict):
+                return (6, 'Each record must be a dictionary')
+            if "type" not in record and "type" not in group:
+                return (7, 'Each record must have a "type" unless the group has a type')
+            if not isinstance(record.get("meta", {}), dict):
+                return (8, 'If "meta" is present on a record, it must be a dictionary')
+            if not isinstance(record.get("references", []), list):
+                return (9, 'If "references" is present on a record, it must be a list')
+            if "references" not in record and not [key for key in record.keys() if key not in ["type", "meta"]]:
+                return (10, 'If "references" is not present on a record, it must have other keys besides "type" and "meta"')
+            if "references" in record and [key for key in record.keys() if key not in ["type", "meta", "references"]]:
+                return (11, 'If "references" is present on a record, it must not have other keys besides "type", "meta", and "references"')
+            for key, value in record.items():
+                if key not in ["type", "meta", "references"]:
+                    # unit role
+                    result = validate_reference_unit(value)
+                    if result is not None:
+                        return result
+            if "references" in record:
+                if record["references"] == []:
+                    return (14, 'A references list cannot be empty')
+                for reference_unit in record["references"]:
+                    result = validate_reference_unit(reference_unit)
+                    if result is not None:
+                        return result
 
     return True
